ef8db5eea8475e5dbdf3167b4c1771baaf513ab6
jabeermk <jabeermk@gmail.com>
2020-05-05 18:20:22 +0530

    initialize heroku deployment6:20

--- server/index.js
+++ server/index.js
@@ -92,7 +92,7 @@ app.get('/api/user/logout', auth, (req, res) => {
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server Runninng at ${port}`)
 });
 
