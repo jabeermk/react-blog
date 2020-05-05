086243ab7d4ac3a916501524472134587678941a
jabeermk <jabeermk@gmail.com>
2020-05-05 11:04:02 +0530

    bcrypt

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const config = require('./config/key')
 
 
 const { User } = require('./models/user');
+const { auth } = require('./middleware/auth');
 
 mongoose.connect(config.mongoURI, {
     useUnifiedTopology: true,
@@ -19,6 +20,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/api/user/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+
+})
 
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
@@ -32,7 +44,7 @@ app.post('/api/users/register', (req, res) => {
     })
 })
 
-app.post('/api/users/login', (req, res) => {
+app.post('/api/user/login', (req, res) => {
     //find the email
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user)
@@ -48,19 +60,26 @@ app.post('/api/users/login', (req, res) => {
 
         })
         //generate token
-        user.generateToken((err,user)=>{
-            if(err) return res.status(400).send(err);
-            res.cookie("x_auth",user.token)
-            .status(200)
-            .json({
-                loginSuccess:true
-            })
-        })
-    })
-
-
-    
+        user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true
+                });
+        });
+    });
 });
+
+app.get('/api/user/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, doc) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).send({
+            success: true
+        });
+    });
+});
+
 // app.get('/', (req, res) => {
 //     res.send('hello world')
 // });
